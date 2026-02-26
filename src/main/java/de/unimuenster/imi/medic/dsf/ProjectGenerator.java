a1c453cc1c64aee32e70aed3cc937c645b0f998b
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2026-02-26 11:37:52 +0100

    Implemented Project Generator task

---
+++ src/main/java/de/unimuenster/imi/medic/dsf/ProjectGenerator.java
@@ -0,0 +1,25 @@
+package de.unimuenster.imi.medic.dsf;
+
+import de.unimuenster.imi.medic.dsf.shared.DsfProjectDTO;
+import de.unimuenster.imi.medic.dsf.utils.DbGenerator;
+import de.unimuenster.imi.medic.dsf.utils.DockerGenerator;
+import de.unimuenster.imi.medic.dsf.utils.FolderGenerator;
+import de.unimuenster.imi.medic.dsf.utils.KeycloakGenerator;
+import de.unimuenster.imi.medic.dsf.utils.PomGenerator;
+import de.unimuenster.imi.medic.dsf.utils.ProxyGenerator;
+import de.unimuenster.imi.medic.dsf.utils.SecretsGenerator;
+
+public class ProjectGenerator {
+
+    public void generate(DsfProjectDTO projectDTO) {
+        FolderGenerator gen = new FolderGenerator();
+        gen.generateProjectFiles(projectDTO);
+        SecretsGenerator.generateSecrets(projectDTO);
+        DockerGenerator.generateDockerFile(projectDTO);
+        DbGenerator.generateDb(projectDTO);
+        KeycloakGenerator.generateKeycloakImport(projectDTO);
+        ProxyGenerator.generateProxy(projectDTO);
+        PomGenerator.generatePomFile(projectDTO);
+    }
+
+}
