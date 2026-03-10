388dc6ac0e27511f4d9e2682b76b218256ae3e53
sprus <sascha.prus@uni-muenster.de>
2026-03-10 13:45:20 +0100

    -> revised ProxyGenerator

--- src/main/java/utils/ProxyGenerator.java
+++ src/main/java/utils/ProxyGenerator.java
@@ -1,12 +1,17 @@
 package utils;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import shared.DsfOrganizationDTO;
 import shared.DsfProjectDTO;
 
 import java.io.File;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Collections;
+import java.util.HashMap;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
@@ -14,18 +19,17 @@ public class ProxyGenerator {
 
     public static boolean generateProxy(DsfProjectDTO dsfProjectDTO) {
         try {
-            File proxyConf = new File("./src/main/resources/proxy/proxy.conf");
             for (DsfOrganizationDTO organization : dsfProjectDTO.getOrganizations()) {
-                String proxyStr = Files.readString(proxyConf.toPath());
-                String organizationName = organization.getNamespace();
-                String proxyOrganizationStr = proxyStr.replace("{ organizationName }", organizationName)
-                        .replace("{ frontendFhirIp }", organization.getIpConfig().getFhirFrontendIp())
-                        .replace("{ frontendBpeIp }", organization.getIpConfig().getBpeFrontendIp());
+                HashMap<String, Object> config = new HashMap<>();
+                config.put("organization", organization);
+                MustacheFactory mf = new DefaultMustacheFactory();
+                Mustache mustache = mf.compile("proxy/proxy.mustache");
+                StringWriter writer = new StringWriter();
+                mustache.execute(writer, config).flush();
                 File proxyOrganizationFile = new File(dsfProjectDTO.getOutputPath() + File.separator +
-                        "dev-setup/proxy/conf.d/"  + organizationName + ".conf");
+                        "dev-setup/proxy/conf.d/"  + organization.getNamespace() + ".conf");
                 Files.write(proxyOrganizationFile.toPath(),
-                        Collections.singleton(proxyOrganizationStr),
-                        StandardCharsets.UTF_8);
+                        writer.toString().getBytes());
             }
             File nginxConf = new File("./src/main/resources/proxy/nginx.conf");
             File nginxConfFile = new File(dsfProjectDTO.getOutputPath() + File.separator +
